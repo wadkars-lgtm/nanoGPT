ebcbe1f74124fe3d47c4f633b5532726c7d1a660
wadkars-lgtm <wadkar.sameer.github@gmail.com>
2025-12-28 09:51:57 -0500

    Make it runnable on RTX 5070

--- bench.py
+++ bench.py
@@ -12,11 +12,11 @@ from model import GPTConfig, GPT
 batch_size = 12
 block_size = 1024
 bias = False
-real_data = True
+real_data = False
 seed = 1337
 device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
 dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32' or 'bfloat16' or 'float16'
-compile = True # use PyTorch 2.0 to compile the model to be faster
+compile = False # use PyTorch 2.0 to compile the model to be faster
 profile = False # use pytorch profiler, or just simple benchmarking?
 exec(open('configurator.py').read()) # overrides from command line or config file
 # -----------------------------------------------------------------------------
